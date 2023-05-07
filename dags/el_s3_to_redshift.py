e67037ca2126beabedd0dc881933f50cdd853538
TanLe14 <104408349+TanLe14@users.noreply.github.com>
2023-05-07 14:51:16 +0700

    Update el_s3_to_redshift.py

--- dags/el_s3_to_redshift.py
+++ dags/el_s3_to_redshift.py
@@ -10,7 +10,7 @@ from sqlalchemy import create_engine
 import time
 from airflow.operators.email import EmailOperator
 import smtplib
-
+#git test
 #Get variable in airflow.
 months=Variable.get('months')
 year=Variable.get('year')
